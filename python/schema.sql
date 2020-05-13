393b2d81ba29904c8076df0ab4bd68c92c220786
Simson L. Garfinkel <simsong@acm.org>
2020-05-12 21:53:26 -0400

    Update schema.sql

--- python/schema.sql
+++ python/schema.sql
@@ -1,1 +1,18 @@
 ;; MySQL Schema
+
+CREATE TALBE weblog (
+  id int not null auto_increment,
+  ipaddr varchar(64) not null,
+  when datetime not null,
+  request varchar(255) not null,
+  user varchar(255),
+  referrer varchar(255),
+  agent varchar(255),
+  PRIMARY KEY (id),
+  INDEX (ipaddr),
+  INDEX (when),
+  INDEX (user),
+  INDEX (referrer),
+  INDEX (agent)
+)
+         
