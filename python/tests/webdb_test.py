c1a6264c6e3907bb3945639731d13df807f6d6c3
joedinsmoor <55299247+joedinsmoor@users.noreply.github.com>
2020-06-24 17:55:47 -0400

    Update webdb_test.py

--- python/tests/webdb_test.py
+++ python/tests/webdb_test.py
@@ -20,11 +20,11 @@ size int(6) NOT NULL,)"""
 
 goal = testcursor.execute(table_create)
 print("table created")
-#parse log
+
 parse_log = ("INSERT INTO testTable "
 "(ipaddr, ident, user, datetime, request, result, size) "
 "VALUES (%s, %s, %s, %s, %s, %s, %s)")
-#Ensure data is committed to database
+
 
 test.commit()
 
