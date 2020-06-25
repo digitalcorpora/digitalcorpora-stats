27cdad2b14ce055c616d47d42d651b626ec252cf
Simson Garfinkel <simsong@acm.org>
2020-06-24 20:13:07 -0400

    You can now run 'make check' in the root directory to run all the checks:
    ```
    -*- mode: compilation; default-directory: "~/gits/digitalcorpora-stats/" -*-
    Compilation started at Wed Jun 24 20:12:47
    
    make -k check
    py.test python
    ================================================================ test session starts ================================================================
    platform darwin -- Python 3.7.6, pytest-5.3.5, py-1.8.1, pluggy-0.13.1
    rootdir: /Users/simsong/gits/digitalcorpora-stats
    plugins: cov-2.9.0, hypothesis-5.5.4, arraydiff-0.3, remotedata-0.3.2, openfiles-0.4.0, doctestplus-0.5.0, astropy-header-0.1.2
    collected 4 items
    
    python/tests/parser_test.py ...                                                                                                                [ 75%]
    python/tests/schema_test.py .                                                                                                                  [100%]
    
    ================================================================= 4 passed in 0.15s =================================================================
    flake8 python --count --select=E9,F63,F7,F82 --show-source --statistics
    0
    flake8 python
    flake8 python --count --exit-zero --max-complexity=10 --statistics
    0
    
    Compilation finished at Wed Jun 24 20:12:49
    ```

--- Makefile
+++ Makefile
@@ -1,2 +1,6 @@
-flake8:
-	flake8
+check:
+	py.test python
+	flake8 python --count --select=E9,F63,F7,F82 --show-source --statistics
+	flake8 python
+	flake8 python --count --exit-zero --max-complexity=10 --statistics
+
--- python/tests/schema_test.py
+++ python/tests/schema_test.py
@@ -8,23 +8,25 @@ import pytest
 import pymysql
 import os
 
-MINIMUM_MYSQL_VERSION='5.7'
+MINIMUM_MYSQL_VERSION = '5.7'
 
 # For info on fixtures, see:
 # https://docs.pytest.org/en/stable/fixture.html
 
+
 @pytest.fixture
 def db_connection():
     conn = pymysql.connect(host=os.environ['TMP_DBWRITER_HOSTNAME'],
-                         database=os.environ['TMP_DBWRITER_DATABASE'],
-                         user=os.environ['TMP_DBWRITER_USERNAME'],
-                         password=os.environ['TMP_DBWRITER_PASSWORD'])
+                           database=os.environ['TMP_DBWRITER_DATABASE'],
+                           user=os.environ['TMP_DBWRITER_USERNAME'],
+                           password=os.environ['TMP_DBWRITER_PASSWORD'])
     yield conn
     conn.close()
+
 
 def test_version(db_connection):
     cursor = db_connection.cursor()
     cursor.execute('SELECT version();')
     rows = cursor.fetchall()
-    assert len(rows)==1
+    assert len(rows) == 1
     assert rows[0][0] >= MINIMUM_MYSQL_VERSION
