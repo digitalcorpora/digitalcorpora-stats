c21ce9ba412eb9ad8cd2525ba9245e92d774f429
Joe Dinsmoor <55299247+joedinsmoor@users.noreply.github.com>
2020-07-03 11:02:37 -0400

    added json building capability

---
+++ python/tests/webdb_test.py
@@ -0,0 +1,47 @@
+import weblog
+import mysql.connector
+from datetime import date, datetime, timedelta
+
+
+testdb = mysql.connector.connect(
+host="127.0.0.1",
+user="test",
+password="testtest123",
+)
+
+cursor = testdb.cursor
+
+#cursor.execute("CREATE TABLE testTable")
+
+#parse log
+parse_log = ("INSERT INTO testTable "
+            "(ipaddr, ident, user, datetime, request, result, size) "
+            "VALUES (%s, %s, %s, %s, %s, %s, %s)")
+
+#Ensure data is committed to database
+testdb.commit()
+
+#cursor.close()
+testdb.close()
+
+from genson import SchemaBuilder
+builder = SchemaBuilder()
+builder.add_schema({"type":"object", "properties":{}})
+builder.add_object({"hi":"there"})
+builder.add_object({"hi":5})
+
+builder.to_schema()
+{'$schema': "$linktoschema",
+"type": "object",
+"properties": {
+"hi": {
+"type": [
+"integer",
+"string"
+]
+}
+},
+"required": [
+"hi"
+]
+}
