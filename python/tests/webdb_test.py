85f13dc12521642ee304053ca50ac5756d2c2368
Joe Dinsmoor <55299247+joedinsmoor@users.noreply.github.com>
2020-07-03 11:04:44 -0400

    Update webdb_test.py

--- python/tests/webdb_test.py
+++ python/tests/webdb_test.py
@@ -11,24 +11,24 @@ password="testtest123",
 
 cursor = testdb.cursor
 
-#cursor.execute("CREATE TABLE testTable")
+# cursor.execute("CREATE TABLE testTable")
 
-#parse log
+# parse log
 parse_log = ("INSERT INTO testTable "
             "(ipaddr, ident, user, datetime, request, result, size) "
             "VALUES (%s, %s, %s, %s, %s, %s, %s)")
 
-#Ensure data is committed to database
+# Ensure data is committed to database
 testdb.commit()
 
-#cursor.close()
+# cursor.close()
 testdb.close()
 
 from genson import SchemaBuilder
 builder = SchemaBuilder()
-builder.add_schema({"type":"object", "properties":{}})
-builder.add_object({"hi":"there"})
-builder.add_object({"hi":5})
+builder.add_schema({"type": "object", "properties": {}})
+builder.add_object({"hi": "there"})
+builder.add_object({"hi": 5})
 
 builder.to_schema()
 {'$schema': "$linktoschema",
