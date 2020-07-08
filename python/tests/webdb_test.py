03aa925e4b24e360b5a66c0106622628fa6c0a16
Joe Dinsmoor <55299247+joedinsmoor@users.noreply.github.com>
2020-07-08 11:37:40 -0400

    switched to PyMySQL, fixed genson errors

--- python/tests/webdb_test.py
+++ python/tests/webdb_test.py
@@ -1,47 +1,47 @@
-# import weblog
-import mysql.connector
-# from datetime import date, datetime, timedelta
+import weblog
+import pymysql
+from datetime import date, datetime, timedelta
 from genson import SchemaBuilder
 
-testdb = mysql.connector.connect(
-    host="127.0.0.1",
-    user="test",
-    password="testtest123",
+connection = pymysql.connect(
+    host='127.0.0.1',
+    user='test',
+    password='testtest123',
+    charset='utf8mb4',
+    cursorclass=pymysql.cursors.DictCursor
 )
 
-cursor = testdb.cursor
+try:
+    with connection.cursor() as cursor:
+        cursor.execute("CREATE TABLE testTable")
+        sql = ("INSERT INTO testTable "
+               "(ipaddr, ident, user, datetime, request, result, size) "
+               "VALUES (%s, %s, %s, %s, %s, %s, %s)")
+        cursor.execute(sql, (weblog.parser))
+        # Ensure data is committed to database
+    connection.commit()
 
-# cursor.execute("CREATE TABLE testTable")
-
-# parse log
-parse_log = ("INSERT INTO testTable "
-             "(ipaddr, ident, user, datetime, request, result, size) "
-             "VALUES (%s, %s, %s, %s, %s, %s, %s)")
-
-# Ensure data is committed to database
-testdb.commit()
-
-# cursor.close()
-testdb.close()
+finally:
+    connection.close()
 
 
-builder = SchemaBuilder()
-builder.add_schema({"type": "object", "properties": {}})
-builder.add_object({"hi": "there"})
-builder.add_object({"hi": 5})
+# builderA = SchemaBuilder()
+# builderA.add_schema({"type": "object", "properties": {}})
+# builderA.add_object({"hi": "there"})
+# builderA.add_object({"hi": 5})
 
-builder.to_schema()
-{'$schema': "$linktoschema",
- "type": "object",
- "properties": {
-     "hi": {
-         "type": [
-            "integer",
-            "string"
-         ]
-     }
- },
-    "required": [
-    "hi"
- ]
- }
+# builderA.to_schema()
+# {'$schema': "$linktoschema",
+ # "type": "object",
+ # "properties": {
+#     "hi": {
+    #     "type": [
+    #        "integer",
+    #        "string"
+    #     ]
+#     }
+ # },
+#    "required": [
+#    "hi"
+ # ]
+ # }
